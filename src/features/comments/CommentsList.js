1c8b74d95d166d63db301a47e018a34fbc8a07fe
Kassandra Salazar <kassandra.salazar@outlook.com>
2023-04-04 22:42:21 -0400

    redux campsiteslice

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,6 +1,7 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
@@ -9,9 +10,7 @@ const CommentsList = ({ campsiteId }) => {
         return (
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
-                })}
+                <CommentForm campsiteId={campsiteId}/>
             </Col>
         );
     }
